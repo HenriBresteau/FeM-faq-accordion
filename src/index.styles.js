27a6adfe7902634dd1b45f62c3766779c72afec3
Henri Bresteau <henri.bresteau@gamil.com>
2022-03-21 17:11:28 +0100

    add Fonts

--- src/index.styles.js
+++ src/index.styles.js
@@ -1,7 +1,18 @@
 import { createGlobalStyle } from "styled-components";
+import Kumbh from "./fonts/KumbhSans-Regular.ttf";
+import KumbhBold from "./fonts/KumbhSans-Bold.ttf";
 
 export const GlobalStyle = createGlobalStyle`
-  :root{
+
+@font-face {
+  font-family: 'Kumbh Sans';
+  src: url(${Kumbh}) format('truetype');
+}
+@font-face {
+  font-family: 'Kumbh Sans Bold';
+  src: url(${KumbhBold}) format('truetype');
+}
+:root{
 --minWidth: 375px;
 --maxWidth: 1440px;
 
@@ -19,19 +30,29 @@ export const GlobalStyle = createGlobalStyle`
 --font-size: 12px;
 
 --ff:'Kumbh Sans', sans-serif;
---fw-normal :400;
---fw-bold :700;
-  }
-*{box-sizing:border-box;
-font-family:var(--ff)}
+--ff-Bold:'Kumbh Sans Bold', sans-serif;
+--fw-normal:400;
+--fw-bold:700;
+}
+
+*{
+  box-sizing:border-box;
+  font-family:var(--ff)}
+;
+
 body{
   margin: 0;
   padding: 0;
   background: linear-gradient(to top, var(--bg-Blue),var(--bg-Violet));
   h1{
-    font-size: 2rem;
+    font-size: 2.5rem;
+    font-family: var(--ff-Bold);
     font-weight: var(--fw-bold);
-    color:var(--h2-gray) ;
+  }
+  h2{
+    color:var(--h2-gray);
+    font-size: 1rem;
+    font-weight:var(--fw-normal);
   }
   .App{
     display: flex;
@@ -40,6 +61,4 @@ body{
     align-items: center;
   }
 }
-@import url('https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700');
-
 `;
